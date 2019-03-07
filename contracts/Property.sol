636b22d81a82d2013942136ec9da0b8b25cdfe45
Spencer Gray <spencer.gray@prizmmedia.com>
2019-03-06 22:04:33 -0800

    fix

--- contracts/Property.sol
+++ contracts/Property.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.24;
 import "zeppelin-solidity/contracts/token/ERC721/ERC721Token.sol";
 
 contract Property is ERC721Token {
-    address invitedGuest;
+//    address invitedGuest;
 
     constructor(string _name, string _symbol) public ERC721Token(_name, _symbol) {
         //leave empty for now, we've passed through the args above (i.e. super)
@@ -14,21 +14,21 @@ contract Property is ERC721Token {
         _;
     }
 
-    modifier onlyGuest {
-        require(msg.sender == invitedGuest); //condition must be true
-        _; //return to function and continue
-    }
+//    modifier onlyGuest {
+//        require(msg.sender == invitedGuest); //condition must be true
+//        _; //return to function and continue
+//    }
 
-    function inviteGuest(address _guest) external onlyOwner returns(address) {
-        invitedGuest = _guest;
-        return invitedGuest;
-    }
+//    function inviteGuest(address _guest) external onlyOwner(tokenURIs[msg.sender]) returns(address) {
+//        invitedGuest = _guest;
+//        return invitedGuest;
+//    }
 
-    function reserveRoom() external payable onlyGuest returns (bool) {
-        if (msg.value < 1) {
-            revert();
-        }
-    }
+//    function reserveRoom() external payable onlyGuest returns (bool) {
+//        if (msg.value < 1) {
+//            revert();
+//        }
+//    }
 
     function createProperty() external {
         _mint(msg.sender, allTokens.length + 1);
