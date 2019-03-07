5c073e7351bac61f606d7c1f97ec4cd4d3b31742
Christopher Powroznik <chrispow96@gmail.com>
2019-03-06 19:57:41 -0800

    changed ganache settings for the GUI

--- contracts/HellowWorld.sol
+++ contracts/HellowWorld.sol
@@ -1,27 +1,30 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.5.0;
 
 contract HelloWorld {
     //track the owner
     address internal owner;
 
-    event Hello(string _msg);
-
     constructor() public {
-        owner = msg.sender;
+        owner = msg.sender; //deployer of contract
     }
 
+    //log string
+    event Hello(uint256 _msg);
+
+    //protects functions from executing
     modifier onlyOwner {
-        require(msg.sender == owner);
-        _;
+        require(msg.sender == owner); //condition must be true
+        _; //return to function and continue
     }
 
-    function hello(string _msg) external onlyOwner {
-        emit Hello(_msg);
+    //trivial hello function, only owner can call
+    function hello(uint256 _msg) external onlyOwner {
+        emit Hello(_msg); //note emit keyword
     }
 
+    //transfer ownership, only owner can call, returns new owner
     function transferOwnership(address _owner) external onlyOwner returns(address) {
         owner = _owner;
         return owner;
     }
-
 }
--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 const Migrations = artifacts.require("Migrations");
+const HelloWorld = artifacts.require("./HelloWorld.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(HelloWorld);
 };
